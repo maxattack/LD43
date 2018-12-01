6c011c54fe6f71736464b7ddba26d1bbe6afadf5
Esteban Fajardo <esteban@sudor.net>
2018-12-01 15:38:11 -0800

    Fuel Cells Turn on Thrusters, Hammer makes Holes
    Pick up a fuel cell and drop it on a Fuel Base to activate a referenced Machine. Next step is to consume fuel.
    
    Hammers can be picked up, and when dropped they will destroy the floor and create an invisible wall the player cannot step on.

---
+++ Assets/Plugins/FuelBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelBase : MonoBehaviour {
+
+    public Machine[] PoweringMachines;
+    public Transform CellPosition;
+
+    void Start()
+    {
+        CellPosition.position = CellPosition.TransformPoint(CellPosition.position);
+    }
+
+    public Transform StartFueling()
+    {
+        foreach (Machine m in PoweringMachines)
+        {
+            m.Activate();
+        }
+
+        return CellPosition;
+    }
+
+    public void StopFueling()
+    {
+        foreach (Machine m in PoweringMachines)
+        {
+            m.Deactivate();
+        }
+    }
+}
--- Assets/Plugins/Hammer.cs
+++ Assets/Plugins/Hammer.cs
@@ -2,23 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hammer : MonoBehaviour {
+public class Hammer : MonoBehaviour, CrewMember.Pickupable {
+
+    private bool isPickedUp;
+
+    public GameObject InvisibleWall;
 
 	void Start () {
 		
 	}
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter(Collider other)
     {
-        //Debug.Log("hey");
+        
         if (other.gameObject.tag == "Tile")
         {
-            //Debug.Log("nice");
-            other.gameObject.GetComponentInChildren<Rigidbody>().AddForce(Vector3.forward);
-            //Destroy(other.gameObject);
-            //reduce weight by tile weight
-            Destroy(gameObject);
+            //other.gameObject.GetComponentInChildren<Rigidbody>().AddForce(Vector3.forward);
+            Instantiate(InvisibleWall, other.transform.position, other.transform.rotation);
+
+            Destroy(other.gameObject);
+            
+            //Destroy(gameObject.transform.parent.gameObject);
         }
+    }
+
+    Transform CrewMember.Pickupable.RootTransform
+    {
+        get { return transform; }
+    }
+
+    void CrewMember.Pickupable.OnPickup(CrewMember crew)
+    {
+        isPickedUp = true;
+        foreach (var it in GetComponentsInChildren<Collider2D>())
+            it.enabled = false;
+    }
+
+    void CrewMember.Pickupable.OnActionPerformed(CrewMember crew)
+    {
+        crew.TryPutDown();
+    }
+
+    void CrewMember.Pickupable.OnDropoff(CrewMember crew)
+    {
+        Debug.Log("firing");
+        isPickedUp = false;
+        foreach (var it in GetComponentsInChildren<Collider2D>())
+            it.enabled = true;
+
+        Collider2D landingOn = Physics2D.OverlapBox(gameObject.transform.position, Vector2.one * 2, 0f, 0, 0f, 0f);
+        Debug.Log(landingOn.name);
     }
 
 }
