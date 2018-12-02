41b4e7057cbacd13030c642ba3496a6a831ff52b
Esteban Fajardo <esteban@sudor.net>
2018-12-01 16:08:33 -0800

    Fuel cells now extinguish
    While a fuel cell is on a base, the fuel will deplete. When it is out of fuel the machine will shut off.

--- Assets/Plugins/FuelCell.cs
+++ Assets/Plugins/FuelCell.cs
@@ -4,24 +4,61 @@ using UnityEngine;
 
 public class FuelCell : MonoBehaviour {
 
-    public float FuelLevel = 5f;
+    public float MaxFuelLevel = 5f;
+    private float FuelLevel = 5f;
+    public GameObject FuelBar;
+    bool spendingFuel = false;
+    public float spendRate = 0.1f;
+    private FuelBase currentBase;
+
+    void Start()
+    {
+        FuelLevel = MaxFuelLevel;
+    }
 
 	public void OnTriggerEnter(Collider other)
     {
         if(other.GetComponent<FuelBase>() != null)
         {
+            currentBase = other.GetComponent<FuelBase>();
             Transform restTransform = other.GetComponent<FuelBase>().StartFueling();
             transform.parent.transform.position = restTransform.position;
             transform.parent.transform.rotation = restTransform.rotation;
+
+            spendingFuel = true;
+            StartCoroutine("LoseFuel");
         }
     }
 
     public void OnTriggerExit(Collider other)
     {
-        if (other.GetComponent<FuelBase>() != null)
+        if (currentBase != null && other.GetComponent<FuelBase>() == currentBase)
         {
-            other.GetComponent<FuelBase>().StopFueling();
+            currentBase.StopFueling();
+            currentBase = null;
+
+            spendingFuel = false;
+            StopAllCoroutines();
         }
+    }
+
+    IEnumerator LoseFuel()
+    {
+        while(spendingFuel)
+        {
+            if(FuelLevel <= 0)
+            {
+                spendingFuel = false;
+                currentBase.StopFueling();
+            }
+
+            FuelLevel -= spendRate;
+
+            FuelBar.transform.localScale = new Vector3(transform.localScale.x, FuelLevel/ MaxFuelLevel, 1);
+
+            yield return new WaitForSeconds(0.5f);
+        }
+        
     }
 
 }
