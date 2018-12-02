43d275c79e09fe251d756033c51eec82070c3223
Esteban Fajardo <esteban@sudor.net>
2018-12-01 20:25:13 -0800

    Pickups can be dropped down holes, Effects
    Redid the turret, added FX to turret and thruster. Holes now consume pickups

---
+++ Assets/Plugins/Chute.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chute : MonoBehaviour, Slot.Listener
+{
+    void Slot.Listener.SlotFilled(Slot slot, CrewMember.Pickupable pickup)
+    {
+        Destroy(pickup.RootTransform.gameObject);
+    }
+
+    void Slot.Listener.SlotEmptied(Slot slot)
+    {
+        // slot cannot be emptied
+    }
+
+}
---
+++ Assets/Plugins/Turret.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour, Slot.Listener {
+
+    public GameObject gun;
+
+    internal FuelCell cell;
+
+    void Awake()
+    {
+        gun.SetActive(false);
+    }
+
+    void Slot.Listener.SlotFilled(Slot slot, CrewMember.Pickupable pickup)
+    {
+        var fuelCell = pickup.RootTransform.GetComponent<FuelCell>();
+        if (fuelCell)
+        {
+            cell = fuelCell;
+        }
+    }
+
+    void Slot.Listener.SlotEmptied(Slot slot)
+    {
+        cell = null;
+    }
+
+    void Update()
+    {
+        var thrust = cell && cell.HasFuel;
+        if (thrust)
+        {
+            if (!gun.active)
+                gun.SetActive(true);
+
+            cell.DrainFuel(Time.deltaTime);
+
+        }
+        else
+        {
+            if (gun.active)
+                gun.SetActive(false);
+        }
+
+    }
+}
