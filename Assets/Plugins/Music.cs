5436508d35adfc3906bfd7a35a3ab3b1228f901e
Max Kaufmann <max.kaufmann@gmail.com>
2018-12-02 17:40:09 -0800

    music!

---
+++ Assets/Plugins/Music.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Music : MonoBehaviour {
+
+	static Music inst;
+
+	private void Awake() {
+		if (inst) {
+			Destroy(gameObject);
+		} else {
+			inst = this;
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+
+	private void OnDestroy() {
+		if (inst == this)
+			inst = null;
+	}
+
+}
