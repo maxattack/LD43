a6f55fd3764981fd196d43c8d1d1624402bcc746
Max Kaufmann <max.kaufmann@gmail.com>
2018-12-02 17:13:51 -0800

    title screen

---
+++ Assets/Plugins/Title.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour {
+
+	public float titleFrequency = 1f;
+	public float titleRotation = 20f;
+
+	public Transform titleText;
+
+	void Update () {
+		
+		var z = titleRotation * Mathf.Sin(Mathf.PI * 2f * Time.time * titleFrequency);
+		titleText.eulerAngles = new Vector3(0f, 0f, z);
+
+		if (Time.time > 0.5f && Input.anyKeyDown) {
+			SceneManager.LoadScene(1);
+		}
+
+	}
+}
